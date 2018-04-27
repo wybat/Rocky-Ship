7cf02ad8081953b901747f0201d40cf8f7c0b9f6
Unknown <wybatt@live.com>
2018-04-27 15:17:00 -0600

    Main Menu Alpha1
    Main Menu complete: begin work on easy mode and game.

---
+++ Launchy ship/Launchy ship/Assets/Scripts/Main_Menu.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Main_Menu : MonoBehaviour {
+
+	public void playgame1()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void playgame2()
+    {
+        SceneManager.LoadScene(2);
+    }
+    public void playgame3()
+    {
+        SceneManager.LoadScene(3);
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
