8fd3a45d9c059fcc89da1cf1c7a42d35370af97c
Unknown <wybatt@live.com>
2018-05-01 18:59:20 -0600

    Easy_Mode/ control creation
    completed controls

---
+++ Launchy ship/Launchy ship/Assets/Scripts/controls.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class controls : MonoBehaviour {
+
+    public Rigidbody2D rb;
+    public float thrust;
+    public float turnThrust;
+    private float thrustInput;
+    private float turnInput;
+    public float screenLeft;
+    public float screenRight;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //thrust input
+        thrustInput = Input.GetAxis ("Vertical");
+        turnInput = Input.GetAxis ("Horizontal");
+
+        //screen wrap
+        Vector2 newPos = transform.position;
+        if (transform.position.x > screenRight)
+        {
+            newPos.x = screenLeft;
+        }
+        if (transform.position.x < screenLeft)
+        {
+            newPos.x = screenRight;
+        }
+        transform.position = newPos;
+    }
+
+    private void FixedUpdate()
+    {
+        rb.AddRelativeForce (Vector2.up * thrustInput);
+        rb.AddTorque (turnInput);
+    }
+
+}
